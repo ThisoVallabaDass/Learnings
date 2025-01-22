1901cd55075061750c44ff612d64960197e5c052
ThisoVallaBADASS <thisoreos@gmail.com>
2025-01-22 21:04:19 +0530

    Update adddigits.cpp

--- C++/adddigits.cpp
+++ C++/adddigits.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
-public:
+ 
+
     int addDigits(int num) {
-        while (num > 9) { // Continue until the number is a single digit
-            num = num / 10 + num % 10; // Add the digits
+        while (num > 9) { // Continue until the number is a single digit which is greater than 9
+            num = num / 10 + num % 10; // num/10 removes the last number && num%10 adds the last nnumber
         }
         return num;
     }
-};
+
 
 int main() {
-    Solution solution;
+   
 
-    int numTestCases;
-    cout << "Enter the number of test cases: ";
-    cin >> numTestCases;
-
-    for (int t = 0; t < numTestCases; ++t) {
-        cout << "Enter number for test case " << t + 1 << ": ";
+    
         int input;
         cin >> input;
 
-        // Call addDigits function
-        int result = solution.addDigits(input);
+        
+        int result = addDigits(input);// Call addDigits function
 
-        // Display the result
-        cout << "Result for test case " << t + 1 << ": " << result << endl;
+        
+        cout << "Result  " << result << endl;
     }
 
     return 0;
