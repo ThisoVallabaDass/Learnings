cdc4643f10063ce3b7f89da88a7bb114ad108b0c
ThisoVallaBADASS <thisoreos@gmail.com>
2025-01-22 20:55:59 +0530

    Update i_patterns.cpp

--- C++/patterns/i_patterns.cpp
+++ C++/patterns/i_patterns.cpp
@@ -3,28 +3,23 @@ using namespace std;
 
 void nForest(int n) {
     for (int i = 0; i < n; i++) { // Loop for rows
-        for (int j = 0; j <= i; j++) { // Loop for stars in each row
-            cout << "* ";  // Print stars with space
+        for (int j = 0; j <= i; j++) { // Loop for column with i times
+            cout << "* ";  // 
         }
         cout << endl;  // Move to the next line after each row
     }
 }
 
 int main() {
-    int numTestCases;
-    cout << "Enter the number of test cases: ";
-    cin >> numTestCases;
-
-    for (int t = 0; t < numTestCases; ++t) {
-        cout << "Enter the value of n for test case " << t + 1 << ": ";
+    
         int n;
         cin >> n;
 
-        // Call nForest function for each test case
-        cout << "Output for test case " << t + 1 << ":" << endl;
-        nForest(n);
+       
+        cout << "Output  " << t + 1 << ":" << endl;
+        nForest(n); // Call nForest function 
         cout << endl;
-    }
+    
 
     return 0;
 }
