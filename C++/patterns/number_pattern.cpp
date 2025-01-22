18ef169644caacf641c9cb0b2cd609aa45601ec5
ThisoVallaBADASS <thisoreos@gmail.com>
2025-01-22 20:50:14 +0530

    Update number_pattern.cpp

--- C++/patterns/number_pattern.cpp
+++ C++/patterns/number_pattern.cpp
@@ -1,28 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 void nTriangle(int n) {
-    for (int i = 0; i <= n; i++) { // Loop through rows
-        for (int j = 1; j <= i; j++) { // Print the number `i` repeated `i` times
-            cout << i << " ";  
+    for (int i = 0; i <= n; i++) { //for row
+        for (int j = 1; j <= i; j++) { //    for column
+            cout << i << " ";  //printing i=0,1,2,3
         }
         cout << endl;  // Move to the next line after each row
     }
 }
 
 int main() {
-    int numTestCases;
-    cout << "Enter the number of test cases: ";
-    cin >> numTestCases;
-
-    for (int t = 0; t < numTestCases; ++t) {
-        cout << "Enter the value of n for test case " << t + 1 << ": ";
         int n;
         cin >> n;
-
-        // Call nTriangle function for each test case
         cout << "Output for test case " << t + 1 << ":" << endl;
-        nTriangle(n);
+        nTriangle(n); // Call nTriangle function 
         cout << endl;
     }
 
