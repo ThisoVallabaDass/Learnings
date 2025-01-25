6be8ac2ea6b10effe81c9b0e649a9a6889966db7
ThisoVallaBADASS <thisoreos@gmail.com>
2025-01-25 21:29:28 +0530

    Add files via upload

---
+++ C++/display_ascii.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    int n;
+    n=int(ch);//to get the ascii value
+    cout<<"THE ASCII VALUE OF "<<ch<<" is "<<n;
+    
+
+}
