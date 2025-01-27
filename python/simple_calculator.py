c93616d663da1806ffeacf5501b7f682529d4e83
ThisoVallaBADASS <thisoreos@gmail.com>
2025-01-27 18:41:58 +0530

    Add files via upload

---
+++ python/simple_calculator.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b  # Use float division
+
+print("Sum:", sum)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
